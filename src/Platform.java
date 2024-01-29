4996eff9218bffe81e0e748df8709c48c47af28d
philippimmler <philipp.immler@hak-reutte.ac.at>
2024-01-29 13:14:32 +0100

    adding constructor and methods for adding and removing submissions to submission list to class Platform

--- src/Platform.java
+++ src/Platform.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 public class Platform {
+    private ArrayList<Submission> submissionList;
+
+    public Platform(ArrayList<Submission> submissionList) {
+        this.submissionList = submissionList;
+    }
+    public void addSubmission(Submission submission){
+        submissionList.add(submission);
+    }
+    public void removeSubmission(Submission submission){
+        submissionList.remove(submission);
+    }
+    public void removeSubmission(int submissionIdx){
+        submissionList.remove(submissionIdx);
+    }
 }
