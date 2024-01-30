20fde5125368cce1dd96217025f3c267a933fb3a
philippimmler <philipp.immler@hak-reutte.ac.at>
2024-01-30 11:16:13 +0100

    adding javadoc comments to the NewsFeed class

--- src/NewsFeed.java
+++ src/NewsFeed.java
@@ -24,6 +24,11 @@ public class NewsFeed {
         }
         return result;
     }
+
+    /**
+     * increases the like value of a submission by one
+     * @param submission submission, which is liked
+     */
     public void likeSubmission(Submission submission){
         if(submissionList.contains(submission)){
             submission.setLikes(submission.getLikes()+1);
@@ -31,6 +36,12 @@ public class NewsFeed {
             System.err.println(submission + " is not available in news feed");
         }
     }
+
+    /**
+     * adds a comment to a submission
+     * @param submission submission, which is commented
+     * @param comment comment of the submission
+     */
     public void commentSubmission(Submission submission, String comment){
         if(submissionList.contains(submission)){
             submission.addComment(comment);
@@ -38,11 +49,20 @@ public class NewsFeed {
             System.err.println(submission + " is not available in news feed");
         }
     }
+
+    /**
+     * prints all submissions on the console
+     */
     public void printALlSubmissions(){
         for(Submission submission:submissionList){
             System.out.println(submission);
         }
     }
+
+    /**
+     * prints all submission of a user on the console
+     * @param username username of the user
+     */
     public void printSubmissionOfUser(String username){
         for(Submission submission:submissionList){
             if(submission.getUsername().equals(username)){
